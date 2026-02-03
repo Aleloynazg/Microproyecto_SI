7809685621b441015b04cf831672bcc3a755c3ad
Aleloynazg <alexandra.loynaz@correo.unimet.edu.ve>
2026-02-02 21:31:25 -0400

    adelanto widgets

---
+++ lib/cardsWidgets.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'cards.dart';
+
+class CartaWidget extends StatefulWidget {
+  final Cards carta; 
+  final VoidCallback onTap; 
+  
+  const CartaWidget({
+    super.key, 
+    required this.carta, 
+    required this.onTap,
+
+  });
+  @override
+  State<CartaWidget> createState()=>_CartaWidgetState();
+}
+class _CartaWidgetState extends State<CartaWidget>{
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: (){
+        setState((){
+          widget.carta.volteada = true;
+
+        });
+        widget.onTap();},
+        child: Container(
+        margin: const EdgeInsets.all(1), 
+        decoration: BoxDecoration(
+          color: estadoColor(), 
+          borderRadius: BorderRadius.circular(5)
+ 
+        ),
+      child:Center(
+    child: contenidoCarta(),
+  )
+)
+      );
+
+    
+
+  }
+  Widget contenidoCarta(){
+    if(widget.carta.volteada|| widget.carta.encontrada){
+      return Text(
+        widget.carta.valor.toString(), 
+        style: const TextStyle(
+          fontSize: 20,
+        ),
+      );
+    
+    }
+    return const Text('');
+
+  }
+Color estadoColor(){
+  if(widget.carta.encontrada){
+    return Colors.blue;
+  }
+  else if (widget.carta.volteada){
+    return Colors.white;
+
+  }else{
+  return Colors.purple;
+}} 
+}
