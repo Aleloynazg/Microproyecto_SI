f71237cc4cbfcb5cda2d1b7b1c1390f548231cbe
bcarnevali <bcarnevali@correo.unimet.edu.ve>
2026-02-03 08:47:54 -0400

    Bettina: Tablero 6x6 funcionando e interfaz base

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:microproyecto_si/tablero.dart';
+import 'package:microproyecto_si/cardsWidgets.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Juego de Memoeria',
-      theme: ThemeData.light()
+      theme: ThemeData.light(),
       home: const BoardMemory(),
     );
   }
@@ -31,6 +33,39 @@ class BoardMemory extends StatefulWidget {
 
 class _BoardMemoryState extends State<BoardMemory> {
   final Tablero tablero = Tablero();
+
+  //Funcion para manejar el click 
+  void _manejarVolteo(int index){
+    if (tablero.puedeVoltear(index)){
+      setState((){
+        tablero.voltearCarta(index);
+      });
+
+      if (tablero.cartasVolteadas.length == 2){
+        Future.delayed(const Duration (milliseconds:600),(){
+          if (mounted){
+            setState((){
+              _verificarPareja();
+            });
+          }
+        });
+      }
+    }
+  }
+
+  void _verificarPareja(){
+    int i1= tablero.cartasVolteadas[0];
+    int i2 = tablero.cartasVolteadas[1];
+
+    if (tablero.cartas[i1].valor == tablero.cartas [i2].valor){
+      tablero.cartas[i1].encontrada = true;
+      tablero.cartas[i2].encontrada = true;
+      } else{
+        tablero.cartas[i1].encontrada = false;
+        tablero.cartas[i2].encontrada = false;
+      }
+      tablero.cartasVolteadas.clear();
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -42,18 +77,33 @@ class _BoardMemoryState extends State<BoardMemory> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10), 
+            padding: const EdgeInsets.symmetric(vertical: 20), 
             child: Text(
               // Aqui falta poner como es que se pone el cuadradito de intentos 
               'Intentos' , 
-              style: const TextStyle(fontSize: 20),
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ),
       Expanded(
-        child:GridView.builder(gridDelegate: gridDelegate, itemBuilder: itemBuilder)
-
-      )
-        ];
-        )
-}
+  child: GridView.builder(
+    padding: const EdgeInsets.all(10),
+    // Configuración 6x6
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 6, // 6 columnas
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+    ),
+    itemCount: tablero.cartas.length,
+    itemBuilder: (context, index) {
+      return CartaWidget(
+        carta: tablero.cartas[index],
+        onTap: () => _manejarVolteo(index),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
