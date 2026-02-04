e4f8f818b12e6e1161d54ea5ff8100d0b6e90071
bcarnevali <bcarnevali@correo.unimet.edu.ve>
2026-02-04 09:49:38 -0400

    Iconos en blanco, colores y tamaño corregido

--- lib/cardsWidgets.dart
+++ lib/cardsWidgets.dart
@@ -21,10 +21,14 @@ class _CartaWidgetState extends State<CartaWidget>{
       onTap: (){
         widget.onTap();},
         child: Container(
-        margin: const EdgeInsets.all(1), 
+        margin: const EdgeInsets.all(2), 
         decoration: BoxDecoration(
           color: estadoColor(), 
-          borderRadius: BorderRadius.circular(5)
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.5),
+            width:3,
+          ),
  
         ),
       child:Center(
@@ -36,50 +40,48 @@ class _CartaWidgetState extends State<CartaWidget>{
     
 
   }
-  Widget contenidoCarta(){
-    if(widget.carta.volteada|| widget.carta.encontrada){
-      return Stack(
-        children: [
-          Positioned.fill(
-          
+
+  Widget contenidoCarta() {
+  if (widget.carta.volteada || widget.carta.encontrada) {
+    return Stack(
+      children: [
+        Positioned.fill(
           child: Center(
             child: FittedBox(
               fit: BoxFit.contain,
-             child: Icon(widget.carta.icono, color: Colors.black
+              child: Icon(
+                widget.carta.icono, 
+                color: Colors.white,
+                size: 70, 
+              ),
             ),
           ),
-            ),),
-        Positioned(
-          top: 2, 
-          right: 4,
-          child: SizedBox(width: 10, height: 10,
-          child: FittedBox(
-            fit: BoxFit.scaleDown,
-          child: Text
-          (widget.carta.valor.toString(),
-           style: const TextStyle(
-          fontSize: 20),),
-        
         ),
+        Positioned(
+          top: 2,
+          right: 4,
+          
+          child: Text(
+            widget.carta.valor.toString(),
+            style: const TextStyle(
+              fontSize: 14, 
+              fontWeight: FontWeight.bold,
+              color: Colors.white70,
+            ),
           ),
         ),
-      
-          
-        ],
-      );
-    
-    }
-    return const SizedBox();
-
+      ],
+    );
   }
+  return const SizedBox();
+}
 Color estadoColor(){
   if(widget.carta.encontrada){
-    return Colors.green; //Cambiarlo a verde cuando ya esten en pareja
+    return Colors.green; // Cambiarlo a verde cuando ya esten en pareja
   } else if (widget.carta.volteada){
-    return Colors.white;
-
-  }else{
-  return Colors.purple;
-    }
+    return Colors.primaries[widget.carta.valor % Colors.primaries.length];
+  } else {
+    return Colors.purple; // Color de la carta boca abajo
   }
+}
 }
